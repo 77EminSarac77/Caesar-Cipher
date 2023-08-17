4c7c51b60cef4a7673311abad8170a7c6866a885
Emin Sarac <124945009+77EminSarac77@users.noreply.github.com>
2023-08-17 13:41:45 -0500

    Add files via upload

---
+++ CaesarCipher.py
@@ -0,0 +1,62 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""""
+
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            ]
+
+
+def caesar(message, num_shift, encrypt_decrypt):
+    copy_alphabet = alphabet.copy()
+    change_text = ""
+    keep = False
+    if encrypt_decrypt == "decode":
+        num_shift *= -1
+    for index in range(0, len(message)):
+        letter = message[index]
+        if letter not in alphabet:
+            keep = True
+        if not keep:
+            shifted_letter = copy_alphabet.pop(num_shift + alphabet.index(letter))
+            change_text += shifted_letter
+            copy_alphabet = alphabet.copy()
+        if keep:
+            change_text += letter
+        keep = False
+    print(f"The {encrypt_decrypt}d text is", change_text)
+
+
+def main():
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar(message=text, num_shift=shift, encrypt_decrypt=direction)
+    rerun_program = input("Type \'yes\' if you want to go again. Otherwise type \'no\'.\n")
+    while rerun_program == 'yes':
+        main()
+    print("Goodbye")
+    exit()
+
+
+main()
+
+
